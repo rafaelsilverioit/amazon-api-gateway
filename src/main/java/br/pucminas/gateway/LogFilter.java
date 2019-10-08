56502f43261d5df2ae9e260dc4815bceb16526f4
Rafael Silvério <rafael.silverio11@gmail.com>
2019-10-08 01:59:39 -0300

    Adding Zuul as our API Gateway.

---
+++ src/main/java/br/pucminas/gateway/LogFilter.java
@@ -0,0 +1,37 @@
+package br.pucminas.gateway;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class LogFilter extends ZuulFilter {
+	private static Logger log = LoggerFactory.getLogger(LogFilter.class);
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+
+		log.info(String.format("%s %s", request.getMethod(), request.getRequestURL().toString()));
+		return null;
+	}
+	
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+}
